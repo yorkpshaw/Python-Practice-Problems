c4363ccd70fe9876beaa8f925e6dde6090899ed1
York <york.p.shaw@gmail.com>
2022-08-21 18:25:51 -0500

    Completed 020

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -2,5 +2,26 @@
 # of the people in the attendees list is greater than or equal
 # to 50% of the number of people in the members list.
 
+
 def has_quorum(attendees_list, members_list):
-    pass
+    # get number of people in attendees list
+    num_attendees = len(attendees_list)
+    print(num_attendees)
+
+    # get the value of half of the members list
+    num_members = len(members_list)
+    print(num_members)
+
+    # do a comparison of num attendees and to 50% of members
+
+    if num_attendees / num_members >= 0.5:
+        return True
+    else:
+        return False
+    # use >=
+
+
+print(has_quorum(["Bart", "Jay", "Terra", "York"], ["Onyx", "Catman"]))
+print(has_quorum(["Bart", "Jay"], ["Onyx", "Catman", "Terra", "York"]))
+print(has_quorum(["Bart"], ["Onyx", "Catman", "Jay", "Terra", "York"]))
+print(has_quorum(["Bart", "Jay", "Terra", "York", "Onyx"], ["Catman"]))
