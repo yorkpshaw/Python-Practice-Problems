5c682569e42ba4fd7265ecca5debab235422b878
York <york.p.shaw@gmail.com>
2022-08-22 14:18:31 -0500

    Completed 010

--- problems/problem_010.py
+++ problems/problem_010.py
@@ -10,5 +10,23 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+
+# return "fizz" if number is divisible by 3
+# use a divider operator to check if number / 3 == 0
+# if it is, return fizz
+# if the number != 3, then return the number
+
+
 def is_divisible_by_3(number):
-    pass
+    if number % 3 == 0:
+        return "Fizz"
+    else:
+        return number
+
+
+print(is_divisible_by_3(3))
+print(is_divisible_by_3(6))
+print(is_divisible_by_3(1))
+print(is_divisible_by_3(10))
+print(is_divisible_by_3(66))
+print(is_divisible_by_3(100))
