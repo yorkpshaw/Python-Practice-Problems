fdaf8d632b1e7420039f599f052e23ac57d164a5
York <york.p.shaw@gmail.com>
2022-08-22 14:35:42 -0500

    Completed 012

--- problems/problem_012.py
+++ problems/problem_012.py
@@ -15,5 +15,27 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+# check if number % 3 and number % 5 == 0; if so, return fizzbuzz
+# if number only divisible by 3, return fizz
+# if number only divisible by 5, return buzz
+
+
 def fizzbuzz(number):
-    pass
+    if (number % 3 == 0) and (number % 5 == 0):
+        return "fizzbuzz"
+    elif number % 3 == 0:
+        return "fizz"
+    elif number % 5 == 0:
+        return "buzz"
+    else:
+        return number
+
+
+print(fizzbuzz(6))
+print(fizzbuzz(15))
+print(fizzbuzz(25))
+print(fizzbuzz(29))
+print(fizzbuzz(36))
+print(fizzbuzz(45))
+print(fizzbuzz(50))
+print(fizzbuzz(67))
