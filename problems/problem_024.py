c07443e94b6195d8504b8c5f019b586f75e741e1
York <york.p.shaw@gmail.com>
2022-09-01 21:34:31 -0500

    Completed 024

--- problems/problem_024.py
+++ problems/problem_024.py
@@ -7,5 +7,18 @@
 #
 # Pseudocode is available for you
 
+
 def calculate_average(values):
-    pass
+    if len(values) == 0:
+        return None
+    total = 0
+    for value in values:
+        total += value
+        avg = total / len(values)
+
+    return avg
+
+
+print(calculate_average([1, 2, 3, 4, 5]))
+print(calculate_average([5, 9, 10, 17, 25]))
+print(calculate_average([5, 5, 5, 5, 5]))
