908c217ebb4f5d80591d2a41d2e7055b4a962045
York <york.p.shaw@gmail.com>
2022-09-04 12:27:21 -0500

    Completed 030

--- problems/problem_030.py
+++ problems/problem_030.py
@@ -11,5 +11,25 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+
+# if the list is empty, return none
+# if the list only has one value (if length is one) return none
+# sort the list in order
+# pop the last value
+# return the last value of list with -1
+
+
 def find_second_largest(values):
-    pass
+    if len(values) <= 1:
+        return None
+
+    dupes = set(values)
+
+    sorted_nums = list(sorted(dupes))
+
+    return sorted_nums[-2]
+
+
+print(find_second_largest([1, 5, 2, 6, 3, 6]))
+print(find_second_largest([65, 10, 39, 104, 28, 104]))
+print(find_second_largest([-1, -6, -2, -4, -2]))
