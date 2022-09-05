1fee370d76bc03f5c63fd4e286dc787d43d7e83f
York <york.p.shaw@gmail.com>
2022-09-05 14:01:11 -0500

    Completed 060

--- problems/problem_060.py
+++ problems/problem_060.py
@@ -13,3 +13,15 @@
 #     * input:   [1, 3, 5, 7]
 #       returns: [1, 3, 5, 7]
 
+
+def only_odds(num_list):
+    empty_list = []
+    for odd_num in num_list:
+        if odd_num % 2 == 1:
+            empty_list.append(odd_num)
+    return empty_list
+
+
+print(only_odds([1, 2, 3, 4]))
+print(only_odds([2, 4, 6, 8]))
+print(only_odds([1, 3, 5, 7]))
