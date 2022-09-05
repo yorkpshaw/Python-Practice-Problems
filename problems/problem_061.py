40b72e79a7441f2a46e804b5939cc0fd9b543318
York <york.p.shaw@gmail.com>
2022-09-05 14:15:08 -0500

    Completed 061

--- problems/problem_061.py
+++ problems/problem_061.py
@@ -14,3 +14,19 @@
 #     * input:   [1, 3, 3, 20, 3, 2, 2]
 #       returns: [1, 3, 20, 2]
 
+
+def remove_duplicates(list_values):
+
+    lst = []
+
+    for numbers in list_values:
+        if numbers not in lst:
+            lst.append(numbers)
+
+    return lst
+
+
+print(remove_duplicates([1, 1, 1]))
+print(remove_duplicates([1, 2, 1, 2]))
+print(remove_duplicates([1, 2, 2, 1]))
+print(remove_duplicates([1, 3, 3, 20, 3, 2, 2]))
