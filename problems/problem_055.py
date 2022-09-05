f6d162d235cce473d6edf13e815648b9db638611
York <york.p.shaw@gmail.com>
2022-09-05 13:21:40 -0500

    Completed 055

--- problems/problem_055.py
+++ problems/problem_055.py
@@ -28,3 +28,27 @@
 #     * input: 10
 #       returns:  "X"
 
+
+def simple_roman(value):
+    roman_num = {
+        1: "I",
+        2: "II",
+        3: "III",
+        4: "IV",
+        5: "V",
+        6: "VI",
+        7: "VII",
+        8: "VIII",
+        9: "IX",
+        10: "X",
+    }
+
+    return roman_num[value]
+
+
+print(simple_roman(1))
+print(simple_roman(3))
+print(simple_roman(5))
+print(simple_roman(7))
+print(simple_roman(9))
+print(simple_roman(11))
