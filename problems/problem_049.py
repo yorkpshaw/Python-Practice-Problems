50402ec783ae3ca3e49adad7f97ea021dfc2a9eb
York <york.p.shaw@gmail.com>
2022-09-05 16:03:41 -0500

    Completed 049

--- problems/problem_049.py
+++ problems/problem_049.py
@@ -9,3 +9,11 @@
 #      y: 4
 #      result: 7
 
+
+def sum_two_numbers(num1, num2):
+    return num1 + num2
+
+
+print(sum_two_numbers(3, 4))
+print(sum_two_numbers(1, 99))
+print(sum_two_numbers(66, 66))
