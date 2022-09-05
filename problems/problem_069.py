462a8cae2a10a9a22d90b0fdd4f5b98c1dfac2e7
York <york.p.shaw@gmail.com>
2022-09-05 09:17:10 -0500

    Completed 069

--- problems/problem_069.py
+++ problems/problem_069.py
@@ -23,15 +23,45 @@
 
 
 # class Student
-    # method initializer with required state "name"
-        # self.name = name
-        # self.scores = [] because its an internal state
+# method initializer with required state "name"
+# self.name = name
+# self.scores = [] because its an internal state
 
-    # method add_score(self, score)
-        # appends the score value to self.scores
+# method add_score(self, score)
+# appends the score value to self.scores
 
-    # method get_average(self)
-        # if there are no scores in self.scores
-            # return None
-        # returns the sum of the scores divided by
-        # the number of scores
+# method get_average(self)
+# if there are no scores in self.scores
+# return None
+# returns the sum of the scores divided by
+# the number of scores
+
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.scores = []
+
+    def add_score(self, score):
+        self.scores.append(score)
+
+    def get_average(self):
+        if len(self.scores) == 0:
+            return None
+
+        count = 0
+        for sum in self.scores:
+            count += sum
+            average = count / len(self.scores)
+
+        return average
+
+
+student = Student("York")
+print(student.get_average())
+student.add_score(100)
+student.add_score(90)
+print(student.get_average())
+student.add_score(66)
+student.add_score(33)
+print(student.get_average())
