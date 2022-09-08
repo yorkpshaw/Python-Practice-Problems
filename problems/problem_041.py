adbc9d12ac09419a68f6e64374e2acdda19872b7
York <york.p.shaw@gmail.com>
2022-09-08 16:47:45 -0500

    Completed 041

--- problems/problem_041.py
+++ problems/problem_041.py
@@ -20,5 +20,28 @@
 
 # Write out your own pseudocode to help guide you.
 
+
 def add_csv_lines(csv_lines):
-    pass
+    added_nums = []
+
+    for lines in csv_lines:
+        splitnums = lines.split(",")
+        count = 0
+        for numstring in splitnums:
+            sumnum = int(numstring)
+            count += sumnum
+        added_nums.append(count)
+
+    return added_nums
+
+
+print(add_csv_lines(["1,12", "1,9", "33,1"]))
+
+
+# create an empty list
+# loop over the list
+# convert each element to a number, split comma, set that to a new variable
+# loop over the new variable
+# create a varible
+# add the numbers in the converted-to-num string
+# append to the list
