bce386e708b0c108bb986e69dc1145c770ccaaaf
York <york.p.shaw@gmail.com>
2022-09-10 17:06:53 -0500

    Completed 080

--- problems/problem_080.py
+++ problems/problem_080.py
@@ -20,18 +20,46 @@
 
 
 # class Receipt
-    # method initializer with tax rate
-        # self.tax_rate = tax_rate
-        # self.items = new empty list
+# method initializer with tax rate
+# self.tax_rate = tax_rate
+# self.items = new empty list
 
-    # method add_item(self, item)
-        # append item to self.items list
+# method add_item(self, item)
+# append item to self.items list
 
-    # method get_subtotal(self)
-        # sum = 0
-        # for each item in self.items
-            # increase sum by item.get_total()
-        # return sum
+# method get_subtotal(self)
+# sum = 0
+# for each item in self.items
+# increase sum by item.get_total()
+# return sum
 
-    # method get_total(self)
-        # return self.get_subtotal() * (1 + self.tax_rate)
+# method get_total(self)
+# return self.get_subtotal() * (1 + self.tax_rate)
+
+from problem_079 import ReceiptItem
+
+
+class Receipt:
+    def __init__(self, tax_rate):
+        self.tax_rate = tax_rate
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def get_subtotal(self):
+        sum = 0
+        for item in self.items:
+            sum += item.get_total()
+        return sum
+
+    def get_total(self):
+        return self.get_subtotal() * (1 + self.tax_rate)
+
+
+item = Receipt(0.1)
+item.add_item(ReceiptItem(4, 2.50))
+item.add_item(ReceiptItem(2, 5.00))
+
+print(item.get_subtotal())  # Prints 20
+print(item.get_total())  # Prints 22
