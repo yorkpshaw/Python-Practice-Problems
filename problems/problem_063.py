e33183f6aff6d04ad0a8c791063032e4bc79fce5
York <york.p.shaw@gmail.com>
2022-09-10 17:34:29 -0500

    Completed 063

--- problems/problem_063.py
+++ problems/problem_063.py
@@ -21,3 +21,25 @@
 # You may want to look at the built-in Python functions
 # "ord" and "chr" for this problem
 
+
+def shift_letters(str):
+    new_word = ""  # A string will be the result
+
+    for letter in str:  # loop over the string
+        if letter == "Z":  # If letter is Z or z, they become A or a
+            new_letter = "A"
+        elif letter == "z":
+            new_letter = "a"
+        else:  # Use if/elif/else statements to knock out those conditions, then append
+            new_letter = chr(
+                ord(letter) + 1
+            )  # chr will output the letter from a unicode number, ord(letter) represents the unicode number of a letter
+        new_word += new_letter
+
+    return new_word
+
+
+print(shift_letters("import"))
+print(shift_letters("ABBA"))
+print(shift_letters("Kala"))
+print(shift_letters("zap"))
