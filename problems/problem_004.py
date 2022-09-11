3a7e59776cd2a7788b6e2d1966485d85182355e2
York <york.p.shaw@gmail.com>
2022-09-11 18:33:57 -0500

    Completed 4

--- problems/problem_004.py
+++ problems/problem_004.py
@@ -12,5 +12,16 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+
 def max_of_three(value1, value2, value3):
-    pass
+    if value1 >= value2 and value3:
+        return value1
+    elif value2 >= value3:
+        return value2
+    else:
+        return value3
+
+
+print(max_of_three(1, 2, 3))
+print(max_of_three(3, 2, 1))
+print(max_of_three(1, 3, 2))
