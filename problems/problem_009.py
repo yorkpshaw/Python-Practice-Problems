be0bf1353d0edfce57bbd6ccd432b43ee17345d0
York <york.p.shaw@gmail.com>
2022-09-11 20:31:17 -0500

    Completed 9

--- problems/problem_009.py
+++ problems/problem_009.py
@@ -12,5 +12,23 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+
+# set a variable to be the reversed string of word
+# set another variable to be the reversed word variable, with with .join
+# and no spaces
+# if the test case (reversed word of parameter) is equal to the parameter, then return true
+# otherwise, return false
+
+
 def is_palindrome(word):
-    pass
+    reversed_word = reversed(word)
+    new_word = "".join(reversed_word)
+
+    if new_word == word:
+        return True
+    else:
+        return False
+
+
+print(is_palindrome("racecar"))
+print(is_palindrome("nope"))
