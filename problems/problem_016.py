5445b4d916257d7156fa1bc233cb6b69c76eae50
York <york.p.shaw@gmail.com>
2022-09-11 19:51:57 -0500

    Completed 16

--- problems/problem_016.py
+++ problems/problem_016.py
@@ -2,5 +2,16 @@
 # coordinate and a y coordinate, and then tests each to
 # make sure they're between 0 and 10, inclusive.
 
+
 def is_inside_bounds(x, y):
-    pass
+    if x >= 0 and x <= 10 and y >= 0 and y <= 10:
+        return True
+    else:
+        return False
+
+
+print(is_inside_bounds(5, 10))
+print(is_inside_bounds(10, 5))
+print(is_inside_bounds(11, 11))
+print(is_inside_bounds(1, 100))
+print(is_inside_bounds(100, 1))
