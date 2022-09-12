2d49b576c20f7f82ff9e3a9939243c771c8026d7
York <york.p.shaw@gmail.com>
2022-09-11 20:45:17 -0500

    Completed 22

--- problems/problem_022.py
+++ problems/problem_022.py
@@ -6,5 +6,19 @@
 #   * If it is not a workday, the list needs to contain
 #     "surfboard"
 
+
 def gear_for_day(is_workday, is_sunny):
-    pass
+    workday_list = []
+    if is_workday and not is_sunny:
+        workday_list.append("umbrella")
+    elif is_workday:
+        workday_list.append("laptop")
+    else:
+        workday_list.append("surfboard")
+
+    return workday_list
+
+
+print(gear_for_day(True, False))
+print(gear_for_day(True, True))
+print(gear_for_day(False, True))
