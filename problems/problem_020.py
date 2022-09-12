177b0f31128f848ff0bffa420664523d3e78e476
York <york.p.shaw@gmail.com>
2022-09-11 20:15:40 -0500

    Completed 20

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -2,5 +2,23 @@
 # of the people in the attendees list is greater than or equal
 # to 50% of the number of people in the members list.
 
+
 def has_quorum(attendees_list, members_list):
-    pass
+    if len(attendees_list) / len(members_list) >= 0.5:
+        return True
+    else:
+        return False
+
+
+print(has_quorum(["York", "York"], ["York", "York"]))
+print(
+    has_quorum(
+        ["York"],
+        [
+            "York",
+            "York",
+            "York",
+            "York",
+        ],
+    )
+)
