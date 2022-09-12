0fec2074fac600b008f4404e5eb184c4b4527fce
York <york.p.shaw@gmail.com>
2022-09-11 19:12:23 -0500

    Completed 14

--- problems/problem_014.py
+++ problems/problem_014.py
@@ -10,5 +10,13 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+
 def can_make_pasta(ingredients):
-    pass
+    if (
+        "flour" in ingredients
+        and "eggs" in ingredients
+        and "oil" in ingredients
+    ):
+        return True
+    else:
+        return False
