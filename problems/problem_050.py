b16ee37f11c72e97f969ff2a2452341084562ec6
York <york.p.shaw@gmail.com>
2022-09-15 16:36:50 -0500

    Completed 50

--- problems/problem_050.py
+++ problems/problem_050.py
@@ -12,3 +12,26 @@
 #    * input: [1, 2, 3]
 #      result: [1, 2], [3]
 
+
+def halve_the_list(single_list):
+    list1 = []
+    list2 = []
+    if len(single_list) % 2 == 0:
+        mid = len(single_list) // 2
+        list1 = single_list[:mid]
+        list2 = single_list[mid:]
+    if len(single_list) % 2 != 0:
+        mid = (len(single_list) // 2) + 1
+        list1 = single_list[:mid]
+        list2 = single_list[mid:]
+
+    return list1, list2
+
+
+print(halve_the_list([1, 2, 3, 4, 5, 6]))
+
+print(halve_the_list([1, 2, 3, 4, 5, 6, 7]))
+
+# divide the length of the list by 2
+# round up that number
+# append to a list
