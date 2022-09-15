b4ee1b2e2679aa5cf5e2f0e247f56cd1e6e8a16b
York <york.p.shaw@gmail.com>
2022-09-15 17:32:33 -0500

    Completed 51

--- problems/problem_051.py
+++ problems/problem_051.py
@@ -7,3 +7,14 @@
 #
 # Don't for get to import math!
 
+import math
+
+
+def safe_divide(num1, num2):
+    if num2 == 0:
+        return math.inf
+
+    return num1 / num2
+
+
+print(safe_divide(6, 3))
