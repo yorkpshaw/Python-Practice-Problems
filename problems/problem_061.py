fbf9f96fb9964107b8a44c3764d1b50d81aa7adb
York <york.p.shaw@gmail.com>
2022-09-18 16:52:02 -0500

    Completed 61

--- problems/problem_061.py
+++ problems/problem_061.py
@@ -14,3 +14,17 @@
 #     * input:   [1, 3, 3, 20, 3, 2, 2]
 #       returns: [1, 3, 20, 2]
 
+
+def remove_duplicates(values):
+    noduplicates = []
+
+    for char in values:
+        if char not in noduplicates:
+            noduplicates.append(char)
+
+    return noduplicates
+
+
+print(remove_duplicates([1, 1, 1, 1]))
+print(remove_duplicates([1, 2, 2, 1]))
+print(remove_duplicates([1, 3, 3, 20, 3, 2, 2]))
