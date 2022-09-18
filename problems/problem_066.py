15de70e5d7750a48fb2737bbf8b60034d256b192
York <york.p.shaw@gmail.com>
2022-09-17 19:46:10 -0500

    Completed 66

--- problems/problem_066.py
+++ problems/problem_066.py
@@ -20,13 +20,30 @@
 
 
 # class Book
-    # method initializer method with required state
-    # parameters author and title
-        # set self.author = author
-        # set self.title = title
+# method initializer method with required state
+# parameters author and title
+# set self.author = author
+# set self.title = title
 
-    # method get_author(self)
-        # returns "Author: " + self.author
+# method get_author(self)
+# returns "Author: " + self.author
 
-    # method get_title(self)
-        # returns "Title: " + self.title
+# method get_title(self)
+# returns "Title: " + self.title
+
+
+class Book:
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
+
+    def get_author(self):
+        return f"Author: {self.author}"
+
+    def get_title(self):
+        return f"Title: {self.title}"
+
+
+book = Book("York Shaw", "nothing")
+print(book.get_author())
+print(book.get_title())
