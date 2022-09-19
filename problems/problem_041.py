2cbf12d7034848c82444aa1595f489d5ceab8ad1
York <york.p.shaw@gmail.com>
2022-09-18 21:18:04 -0500

    Completed 41

--- problems/problem_041.py
+++ problems/problem_041.py
@@ -20,5 +20,32 @@
 
 # Write out your own pseudocode to help guide you.
 
+
+# create an empty list variable to return
+# loop over the parameter
+# split the list in the variable passed in the loop
+# create a count variable set to 0
+# loop over the split list
+# convert each element in the split list to an integer
+# increment the count variable by the converted integer
+# append the numbers to the result list
+
+
 def add_csv_lines(csv_lines):
-    pass
+
+    result = []
+
+    for csv in csv_lines:
+        lines = csv.split(",")
+        count = 0
+        for line in lines:
+            new_num = int(line)
+            count += new_num
+        result.append(count)
+
+    return result
+
+
+print(add_csv_lines(["8,1,7", "10,10,10", "1,2,3"]))
+print(add_csv_lines([]))
+print(add_csv_lines(["3", "1,9"]))
