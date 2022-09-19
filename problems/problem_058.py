ec34f86e675f4caa4ab7239aed0262426710b4eb
York <york.p.shaw@gmail.com>
2022-09-19 08:16:48 -0500

    Completed 58

--- problems/problem_058.py
+++ problems/problem_058.py
@@ -22,3 +22,57 @@
 #
 # You may want to look up the ".strip()" method for the string.
 
+
+def group_cities_by_state(cities):
+    output = {}
+    for city in cities:
+        name, state = city.split(",")
+        state = state.strip()
+        if state not in output:
+            output[state] = []
+        output[state].append(name)
+    return output
+
+
+# Alternate Solution
+
+
+# def group_cities_by_state(cities):
+#     dict = {}
+#     for city_name in cities:
+#         cityname = city_name.split(", ")
+#         city = cityname[0]
+#         state = cityname[1]
+#         if state not in dict:
+#             dict[state] = [city]  # dict[key] = value
+#         else:
+#             dict[state].append(city)
+#     return dict
+
+
+# dict[key].append(newvalue) is the same as value.append(newvalue)
+
+# If the cities have the same abbreviated state, they belong in the same value
+# to the abbreviated state's key
+# Index 1 in each list will be the dictionary's key
+# Index 0 in each list will be the dictionary's value
+
+# if "key1" in d:
+#     print("this will execute")
+
+# if "nonexistent key" in d:
+#     print("this will not")
+
+
+# if index 1 is not in the dictionary, then set the index 1 to be a key,
+# and the value to be a list
+# If index 1 is in the dictionary,
+
+# Index 0 will go into that and you don't need another index 1
+# append index 0 into the value
+# where is the value of the key
+
+# return a dictionary,
+# the dictionary's key should be the state abbrevation
+# the dictionary's value(s) should be the city name(s)
+# .strip
