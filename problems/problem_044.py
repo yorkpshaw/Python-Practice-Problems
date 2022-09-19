94ac4845c2ead6a2dd930f7afde104f138220945
York <york.p.shaw@gmail.com>
2022-09-18 21:12:35 -0500

    Completed 44

--- problems/problem_044.py
+++ problems/problem_044.py
@@ -17,5 +17,23 @@
 #
 # Remember that a dictionary has the ".get" method on it.
 
+# Return an empty list of Values from a dictionary
+# loop over the list of keys
+# if that key in the list is in the dictionary,
+# use .get to retrieve the values from the dictionary parameter
+# pass the key variable as an argument in .get() to check if there
+# append those values
+
+
 def translate(key_list, dictionary):
-    pass
+    result = []
+
+    for k in key_list:
+        result.append(dictionary.get(k))
+
+    return result
+
+
+print(translate(["name", "age"], {"name": "Noor", "age": 29}))
+print(translate(["eye color", "age"], {"name": "Noor", "age": 29}))
+print(translate(["age", "age", "age"], {"name": "Noor", "age": 29}))
